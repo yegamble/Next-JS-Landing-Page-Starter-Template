7b80b30ec270001d7ee351f9c397cede3ffa5f23
Yosef Gamble <yegamble@gmail.com>
2024-04-13 15:55:22 -0400

    initial commit

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,19 +1,16 @@
 import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
-import { Banner } from './Banner';
-import { Footer } from './Footer';
-import { Hero } from './Hero';
-import { Sponsors } from './Sponsors';
-import { VerticalFeatures } from './VerticalFeatures';
+import Navbar from './Navbar';
 
 const Base = () => (
   <div className="text-gray-600 antialiased">
     <Meta title={AppConfig.title} description={AppConfig.description} />
-    <Hero />
+    <Navbar />
+    {/* <Hero />
     <Sponsors />
     <VerticalFeatures />
     <Banner />
-    <Footer />
+    <Footer /> */}
   </div>
 );
 
--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'iotastreamer',
+  title: 'IOTA Streamer',
+  description: 'A place for videos and entertainment powered by IOTA',
   locale: 'en',
 };
